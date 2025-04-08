6c5483dd7cd15216b415222392ed33669ab005b2
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-04-09 01:21:11 +0530

    work page , about page, home page complete, services page

--- base/urls.py
+++ base/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('service/', views.service, name='service'),
+    path('work/', views.work, name='work'),
+    path('about/', views.about, name='about'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def work(request):
+    return render(request, 'work.html')
+
+def about(request):
+    return render(request, 'about.html')
